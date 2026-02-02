694a8b0265092f7f4966cb7262bc194e1258d52b
Lucio Del Corro Raffetto <delcorroraffetto@gmail.com>
2026-02-02 02:03:40 -0300

    feat: Add cart API routes

---
+++ apps/backend/src/routes/cart-routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import cartController from "../controllers/cart-controller";
+import { authenticate } from "../middlewares/auth-middleware";
+
+const router = Router();
+
+router.use(authenticate);
+router
+  .post("/", cartController.addToCart)
+  .get("/", cartController.getCart)
+  .delete("/", cartController.clearCart)
+  .delete("/:productId", cartController.removeFromCart);
+
+export default router;
