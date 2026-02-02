b832d49bf70eafae60d79a0f028952806b497496
Lucio Del Corro Raffetto <delcorroraffetto@gmail.com>
2026-02-02 02:02:50 -0300

    refactor: Update books custom hook

--- apps/frontend/src/application/useBooks.ts
+++ apps/frontend/src/application/useBooks.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { getBooks } from "../infrastructure/api";
-import { Product } from "@domain/src/entities/Products";
+import type { Product } from "@domain/entities/Products";
 export function useBooks() {
   const [books, setBooks] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
