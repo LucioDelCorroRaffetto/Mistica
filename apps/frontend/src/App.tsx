a87e4a0f682b060b06c8512141db1433515a949c
Lucio Del Corro Raffetto <delcorroraffetto@gmail.com>
2026-02-02 02:02:49 -0300

    refactor: Update main App component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,26 +1,59 @@
 import { Routes, Route } from "react-router-dom";
 import { ProductListPage } from "./page/ListProductPage";
 import { DetailProduct } from "./page/DetailProduct";
-import { ChangoPage } from "./page/ChangoPage";
+import { CartPage } from "./page/CartPage";
+import { HeroPage } from "./page/HeroPage";
+import { ProfilePage } from "./page/ProfilePage";
+import { WishlistPage } from "./page/WishlistPage";
+import { OrdersHistoryPage } from "./page/OrdersHistoryPage";
 import { Navbar } from "./layout/NavBar";
 import { Footer } from "./layout/Footer";
 import { LoginPage } from "./page/LoginPage";
 import { RegisterPage } from "./page/RegisterPage";
 import { AuthGuard } from "./components/authenticate/auth";
+import NotificationCenter from "./components/NotificationCenter";
+import ProductComparator from "./components/ProductComparator";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <NotificationCenter />
+      <ProductComparator />
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<ProductListPage />} />
+          <Route path="/" element={<HeroPage />} />
+          <Route path="/libros" element={<ProductListPage />} />
           <Route path="/products/:productId" element={<DetailProduct />} />
           <Route
-            path="/chango"
+            path="/cart"
             element={
               <AuthGuard>
-                <ChangoPage />
+                <CartPage />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/perfil"
+            element={
+              <AuthGuard>
+                <ProfilePage />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/wishlist"
+            element={
+              <AuthGuard>
+                <WishlistPage />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/orders"
+            element={
+              <AuthGuard>
+                <OrdersHistoryPage />
               </AuthGuard>
             }
           />
