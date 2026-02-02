964b80c2ed5806afcf9aadcb1ca4fb657b05af18
Lucio Del Corro Raffetto <delcorroraffetto@gmail.com>
2026-02-02 02:04:25 -0300

    feat: Add cart service with business logic

---
+++ apps/frontend/src/services/cart-service.ts
@@ -0,0 +1,22 @@
+import api from "./api";
+import type { AddToCartRequest } from "@backend-types/type";
+import type { Cart } from "@domain/entities/Cart";
+
+export const addToCart = async (request: AddToCartRequest) => {
+  const response = await api.post(`/cart`, request);
+  return response.data;
+};
+
+export const getCart = async (): Promise<Cart> => {
+  const response = await api.get(`/cart`);
+  return response.data.payload;
+};
+
+export const removeFromCart = async (productId: string): Promise<Cart> => {
+  const response = await api.delete(`/cart/${productId}`);
+  return response.data.payload;
+};
+
+export const clearCart = async (): Promise<void> => {
+  await api.delete("/cart");
+};
