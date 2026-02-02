4b43f05564505642885e5920751c5168f5d739a1
Lucio Del Corro Raffetto <delcorroraffetto@gmail.com>
2026-02-02 02:03:15 -0300

    refactor: Remove deprecated chango (shopping cart) implementation

--- domain/src/repositories/chango-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Chango } from "../entities/Chango";
-
-export interface ChangoRepository {
-  findChangoByUserId(userId: string): Chango | undefined;
-  saveChango(chango: Chango): void;
-  clearChango(userId: string): boolean;
-  removeChangoItem(userId: string, productId: string): boolean;
-}
